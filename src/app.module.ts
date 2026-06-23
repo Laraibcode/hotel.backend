c39090d3a9749f5e3321c4ba0adc8ecb6f15f378
Laraibcode <laraibnajmi@gmail.com>
2026-06-23 18:46:47 +0500

    minimal app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,7 @@
 import { ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { OrdersModule } from './orders/orders.module';
 import { AdminModule } from './admin/admin.module';
 
-@Module({ imports: [ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]), PrismaModule, AuthModule, OrdersModule, AdminModule] })
+@Module({ imports: [ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]), PrismaModule, AuthModule, AdminModule] })
 export class AppModule {}

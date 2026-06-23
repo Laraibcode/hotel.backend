baa2a0ff5cf1c0ab3060c5223927fb0485d9c656
Laraibcode <laraibnajmi@gmail.com>
2026-06-23 17:44:25 +0500

    fix users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,4 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-
-@Module({ controllers: [UsersController] })
-export class UsersModule {}
+import { Module } from '@nestjs/common'; 
+import { UsersController } from './users.controller'; 
+@Module({ controllers: [UsersController] }) 
+export class UsersModule {} 
